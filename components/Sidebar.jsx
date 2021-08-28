72223c62901234bbabb24b2d934b3d7b60af5d71
unknown <vhuang5564@gmail.com>
2021-08-28 10:02:19 -0700

    add all icon to All Areas

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -2,16 +2,27 @@ import React from "react";
 import styles from '../styles/Sidebar.module.scss';
 import Button from '@material-ui/core/Button';
 import IconButton from "@material-ui/core/Button";
-import SaveIcon  from "@material-ui/icons/Save";
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import AppsIcon from '@material-ui/icons/Apps';
 
 export default function Sidebar(props) {
-  const minimize = console.log("minimized");
+  let n = 0;
+
+  const expand = () => {
+    n = n + 1;
+    console.log(n);
+  };
 
   return (
     <section className={styles.sidebar}>
-      <IconButton startIcon={<ArrowForwardIosIcon />} />
-      <h1 className={styles.sidebar.h1}>This is the Sidebar</h1>
+      <div>
+        <IconButton
+          className={styles.icon}
+          startIcon={<ArrowForwardIosIcon />}
+          onClick={expand}
+        />
+      </div>
+      <h1>All Areas <AppsIcon /></h1>
       <h1>In Room Dining</h1>
       <h1>Restuarant & Lounges</h1>
       <h1>Spa & Salon</h1>
