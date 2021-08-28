90b55561f4b1c8828b15c9111ce13f94160f76de
unknown <vhuang5564@gmail.com>
2021-08-28 10:09:11 -0700

    add concierge icon

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -6,6 +6,8 @@ import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import AppsIcon from '@material-ui/icons/Apps';
 import LocalDiningIcon from '@material-ui/icons/LocalDining';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import MoodIcon from '@material-ui/icons/Mood';
 
 export default function Sidebar(props) {
   let n = 0;
@@ -25,15 +27,15 @@ export default function Sidebar(props) {
         />
       </div>
       <h1>All Areas <AppsIcon /></h1>
-      <h1>In Room Dining <LocalDiningIcon /></h1>
-      <h1>Restuarant & Lounges</h1>
+      <h1>In Room Dining <MenuBookIcon /></h1>
+      <h1>Restuarant & Lounges <LocalDiningIcon /></h1>
       <h1>Spa & Salon</h1>
       <h1>Hotel Amenities</h1>
       <h1>Transport <LocationOnIcon /></h1>
       <h1>Local Flavor</h1>
       <h1>Services</h1>
       <h1>Front-Desk</h1>
-      <h1>Concierge</h1>
+      <h1>Concierge <MoodIcon /></h1>
     </section>
   );
 }
