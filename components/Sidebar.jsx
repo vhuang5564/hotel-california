73bf6b64515028c250041d92ddc1b946547187cf
unknown <vhuang5564@gmail.com>
2021-08-28 08:02:40 -0700

    add arrow icon button

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styles from '../styles/Sidebar.module.scss';
 import Button from '@material-ui/core/Button';
+import IconButton from "@material-ui/core/Button";
+import SaveIcon  from "@material-ui/icons/Save";
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 export default function Sidebar(props) {
   const minimize = console.log("minimized");
 
   return (
     <section className={styles.sidebar}>
+      <IconButton startIcon={<ArrowForwardIosIcon />} />
       <h1 className={styles.sidebar.h1}>This is the Sidebar</h1>
       <h1>In Room Dining</h1>
       <h1>Restuarant & Lounges</h1>
@@ -17,9 +21,6 @@ export default function Sidebar(props) {
       <h1>Services</h1>
       <h1>Front-Desk</h1>
       <h1>Concierge</h1>
-      <Button>
-        Hello World
-      </Button>
     </section>
   );
 }
