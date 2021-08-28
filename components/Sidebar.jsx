ce0d3428ee9f5bf591bdd7b27cf6d3e4abfc3263
unknown <vhuang5564@gmail.com>
2021-08-28 10:05:41 -0700

    add dining Icon

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -4,6 +4,7 @@ import Button from '@material-ui/core/Button';
 import IconButton from "@material-ui/core/Button";
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import AppsIcon from '@material-ui/icons/Apps';
+import LocalDiningIcon from '@material-ui/icons/LocalDining';
 
 export default function Sidebar(props) {
   let n = 0;
@@ -23,7 +24,7 @@ export default function Sidebar(props) {
         />
       </div>
       <h1>All Areas <AppsIcon /></h1>
-      <h1>In Room Dining</h1>
+      <h1>In Room Dining <LocalDiningIcon /></h1>
       <h1>Restuarant & Lounges</h1>
       <h1>Spa & Salon</h1>
       <h1>Hotel Amenities</h1>
