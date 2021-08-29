fd1a02a4a1e8c964a3921b270fc371a671e77e76
unknown <vhuang5564@gmail.com>
2021-08-29 13:29:22 -0700

    add grid template

---
+++ .next/static/webpack/webpack.38c85ac7f38543d24824.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "8eb272d70a3c072f8a71"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay4zOGM4NWFjN2YzODU0M2QyNDgyNC5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O1VBQUEscUNBQXFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS93ZWJwYWNrL3J1bnRpbWUvZ2V0RnVsbEhhc2giXSwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcIjhlYjI3MmQ3MGEzYzA3MmY4YTcxXCI7IH0iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
--- components/Ballroom.jsx
+++ components/Ballroom.jsx
@@ -4,19 +4,19 @@ import styles from '../styles/Ballroom.module.scss';
 
 export default function Ballroom(props) {
   return (
-    <section className={styles.ballroom}>
-      <Button>Ballroom 1</Button>
-      <Button>Ballroom 2</Button>
-      <Button>Ballroom 3</Button>
-      <Button>Ballroom 4</Button>
-      <Button>Ballroom 5</Button>
-      <Button>Ballroom 6</Button>
-      <Button>Ballroom 7</Button>
-      <Button>Ballroom 8</Button>
-      <Button>Ballroom 9</Button>
-      <Button>Ballroom 10</Button>
-      <Button>Ballroom 11</Button>
-      <Button>Ballroom 12</Button>
+    <section className={styles.grid_container}>
+      <Button className={styles.ballroom}>Ballroom 1</Button>
+      <Button className={styles.ballroom}>Ballroom 2</Button>
+      <Button className={styles.ballroom}>Ballroom 3</Button>
+      <Button className={styles.ballroom}>Ballroom 4</Button>
+      <Button className={styles.ballroom}>Ballroom 5</Button>
+      <Button className={styles.ballroom}>Ballroom 6</Button>
+      <Button className={styles.ballroom}>Ballroom 7</Button>
+      <Button className={styles.ballroom}>Ballroom 8</Button>
+      <Button className={styles.ballroom}>Ballroom 9</Button>
+      <Button className={styles.ballroom}>Ballroom 10</Button>
+      <Button className={styles.ballroom}>Ballroom 11</Button>
+      <Button className={styles.ballroom}>Ballroom 12</Button>
     </section>
   );
 }
