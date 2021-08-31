5fcd9558716bf8a5ebbbd29813a90eeda62541ab
BeamMeup-fail <51726921+BeamMeup-fail@users.noreply.github.com>
2021-08-30 19:19:57 -0700

    Added new comments

--- components/AblyReactEffect.js
+++ components/AblyReactEffect.js
@@ -7,15 +7,16 @@ const ably = new Ably.Realtime.Promise({ authUrl: '/api/createTokenRequest' });
 export function useChannel(channelName, callbackOnMessage) {
     const channel = ably.channels.get(channelName);
 
-    const onMount = () => {
+    const onMount = () => { //run each time our component is rendered. Inside onMount, we will subscribe to the specified channel,
+        // triggering callbackOnMessage whenever a message is received.
         channel.subscribe(msg => { callbackOnMessage(msg); });
     }
 
-    const onUnmount = () => {
+    const onUnmount = () => { //unsubscribe from the channel, which will stop accidental multiples of connections, again saving our account limits.
         channel.unsubscribe();
     }
 
-    const useEffectHook = () => {
+    const useEffectHook = () => { //that calls these functions correctly, returning onUnmount for React to use.
         onMount();
         return () => { onUnmount(); };
     };
