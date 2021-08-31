1f19ae8e3833ac6d7b773e79c1ce6ab7ef36d13e
unknown <vhuang5564@gmail.com>
2021-08-31 11:05:06 -0700

    rename css to scss

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import prisma from '../lib/prisma';
 import Link from 'next/link';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Home.module.scss';
 
 
 
