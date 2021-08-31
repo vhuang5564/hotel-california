b38a9297b1264bdbdbe4b9f969ad3346ca1ce2e2
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-08-31 11:15:10 -0600

    Add api to create reviews

---
+++ pages/api/reviews/create.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+import prisma from "../../../lib/prisma"
+
+export default async(req, res) => {
+    const data = JSON.parse(req.body)
+
+    const createdReview = await prisma.review.create({
+        data
+    })
+
+    res.json(createdReview)
+}
