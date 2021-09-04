cb07c56d2f302cd97105b92c618e3b1b62f16fcd
unknown <vhuang5564@gmail.com>
2021-09-04 04:43:52 -0700

    add styling for dashboard

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import prisma from '../../lib/prisma';
 import styles from '../../styles/AdminDashboard.module.scss';
 import PhoneIcon from '@material-ui/icons/Phone';
+import OndemandVideoIcon from '@material-ui/icons/OndemandVideo';
 
 export async function getStaticProps() {
   
@@ -30,13 +31,16 @@ export default function Reviews({data }) {
 
   console.log(data[0].text); // grand.jpg
 
+  // returns icon for specific request takes in string data.text
+  // need to adjust later to include icons of every request
   const request = (text) => {
     switch (text) {
     case 'Need help with audio visual\n' :
       console.log('success!');
-      break;
+      return <OndemandVideoIcon className={styles.container_icon}/>;
     default:
       console.log('failure');
+      return;
     }
   };
 
@@ -48,12 +52,17 @@ export default function Reviews({data }) {
       <Head>
         <title>Reviews Dashboard</title>
       </Head>
+      <header className={styles.header}>
+        <h1>Requests</h1>
+      </header>
       <section className={styles.container}>
         {data?.map((item) => (
           <div key={item.id} className={styles.container_inner}
-            style={{
-              backgroundImage: `url(/${item.ballroom.imageUrl})`
-            }}>
+            // style={{
+            //   backgroundImage: `url(/${item.ballroom.imageUrl})`
+            // }}
+          >
+            {request(item.text)}
             <span>by {item.user?.firstName} </span>
             <span>{item.user?.lastName}</span>
             <br />
@@ -62,7 +71,9 @@ export default function Reviews({data }) {
             <span> {item.text}</span>
             <span> in {item.ballroom?.name}</span>
             <hr />
-            <span>posted at {item.createdAt.slice(11, 16)}</span>
+            <span style={{
+              color: "red",
+            }}>posted at {item.createdAt.slice(11, 16)}</span>
           </div>
         ))}
         <style jsx>{`
@@ -71,6 +82,7 @@ export default function Reviews({data }) {
           padding: 10px;
         }
       `}</style>
+        <div className={styles.total}>Total requests: {data.length}</div>
       </section>
 
     </>
