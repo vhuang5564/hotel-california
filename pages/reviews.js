92fbe409ea28cb8a8f9455b1b8fa0348eba5531a
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-09-09 11:45:47 -0600

    Bring star component for reviews

--- pages/reviews.js
+++ pages/reviews.js
@@ -22,12 +22,14 @@ export async function getServerSideProps(context) {
 
 export default function Review({ data }) {
   const [reviews, setReviews] = useState(data);
-  
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
   async function saveReview(e) {
     e.preventDefault();
     const response = await fetch('/api/reviews/create', {
       body: JSON.stringify({
-        rating: +e.target.rating.value,
+        // rating: +e.target.rating.value,
+        rating: +rating,
         text: e.target.text.value,
       }),
       headers: {
@@ -36,75 +38,83 @@ export default function Review({ data }) {
       },
       method: 'POST',
     });
-    
+
     const review = await response.json();
     setReviews([review, ...reviews]);
     e.target.reset();
   }
-  
 
   return (
-    
-      <Layout title="Hotel California | Leave A Review">
-        <main>
-          <div className={styles.auth}>
-            <h1>How Did We Do Today?</h1>
-            <form onSubmit={saveReview}>
-              <div>
-                <label htmlFor="rating">Rating</label>
-                <select id="rating" name="rating" type="number" required>
-                  <option value="5">5</option>
-                  <option value="4">4</option>
-                  <option value="3">3</option>
-                  <option value="2">2</option>
-                  <option value="1">1</option>
-                </select>
+    <Layout title="Hotel California | Leave A Review">
+      <main>
+        <div className={styles.auth}>
+          <h1 style={{ fontFamily: "'Monoton', cursive", color: "white", fontSize: "300%" }}>
+            Rate Us!
+          </h1>
+          <form onSubmit={saveReview}>
+            <div>
+              <div className="star-rating">
+                {[...Array(5)].map((star, index) => {
+                  index += 1;
+                  return (
+                    <button
+                      type="button"
+                      key={index}
+                      className={
+                        index <= (hover || rating) ? styles.on : styles.off
+                      }
+                      onClick={() => setRating(index)}
+                      onMouseEnter={() => setHover(index)}
+                      onMouseLeave={() => setHover(rating)}
+                    >
+                      <span className="star">&#9733;</span>
+                    </button>
+                  );
+                })}
               </div>
+            </div>
+            <div>
+              {/* <label htmlFor="text">Your Review</label> */}
+              <textarea
+                id="text"
+                name="text"
+                type="textarea"
+                rows="5"
+                placeholder="Your review here... 🏄"
+                required
+              />
+            </div>
+            <input type="submit" value="Post My Review" className="btn" />
+          </form>
+        </div>
+        <section>
+          {reviews?.map((item) => (
+            <div key={item.id} className={styles.item}>
               <div>
-                <label htmlFor="text">Your Review</label>
-                <textarea
-                  // className={styles.item}
-                  id="text"
-                  name="text"
-                  type="textarea"
-                  rows="5"
-                  placeholder="Your review here... 🏄"
-                  required
-                />
-              </div>
-              <input type="submit" value="Post My Review" className="btn" />
-            </form>
-          </div>
-          <section>
-            {reviews?.map((item) => (
-              <div key={item.id} className={styles.item}>
-                <div>
-                  <div className={styles.rating}>rating: {item.rating}</div>
-                  <span
-                    className={styles.rating}
-                    style={{
-                      fontFamily: "'Allison', cursive",
-                      fontSize: '170%',
-                      color: 'red',
-                    }}
-                  >
-                    by {item.user?.firstName} {item.user?.lastName}
-                  </span>
-                  <div className={styles.rating}>
-                    {new Date(item.createdAt).toLocaleDateString('en-US', {
-                      year: 'numeric',
-                      month: 'long',
-                      day: 'numeric',
-                    })}
-                  </div>
+                <div className={styles.rating}>rating: {item.rating}</div>
+                <span
+                  className={styles.rating}
+                  style={{
+                    fontFamily: "'Allison', cursive",
+                    fontSize: '170%',
+                    color: 'red',
+                  }}
+                >
+                 by {item.user ? item.user.firstName : "Jennifer"} {item.user ? item.user.lastName : "Aniston"}
+                </span>
+                <div className={styles.rating}>
+                  {new Date(item.createdAt).toLocaleDateString('en-US', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                  })}
                 </div>
-
-                <div className={styles.rating}>{item.text}</div>
               </div>
-            ))}
-          </section>
-        </main>
-      </Layout>
-    
+              <div className={styles.rating}>{item.text}</div>
+            </div>
+          ))}
+        </section>
+      </main>
+    </Layout>
   );
 }
